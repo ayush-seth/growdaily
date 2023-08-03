1ba0725864879126adc2d23716004c8a0eba2082
Ayush Seth <ayush_seth@icloud.com>
2023-08-03 16:55:17 +0530

    add keyboard close on tap outside

--- screens/OTPVerificationScreen.tsx
+++ screens/OTPVerificationScreen.tsx
@@ -1,5 +1,10 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { StyleSheet, View } from "react-native";
+import {
+  Keyboard,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import OTPTextView from "react-native-otp-textinput";
 import { StackParamList } from "../App";
 import Button from "../components/ui/Button";
@@ -11,28 +16,30 @@ type Props = NativeStackScreenProps<StackParamList, "OTPVerificationScreen">;
 
 const OTPVerificationScreen = ({ navigation }: Props) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.headingContainer}>
-        <Title>Enter your phone number</Title>
-        <Subtitle>OTP sent to XXXXXXXXXX</Subtitle>
-        <OTPTextView
-          autoFocus
-          keyboardType="number-pad"
-          handleTextChange={console.log}
-          containerStyle={styles.otpInput}
-          textInputStyle={styles.otpInputCell}
-          tintColor={colors.primary[500]}
-          offTintColor={colors.borderColor}
-          inputCount={4}
-        />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <View style={styles.headingContainer}>
+          <Title>Enter your phone number</Title>
+          <Subtitle>OTP sent to XXXXXXXXXX</Subtitle>
+          <OTPTextView
+            autoFocus
+            keyboardType="number-pad"
+            handleTextChange={console.log}
+            containerStyle={styles.otpInput}
+            textInputStyle={styles.otpInputCell}
+            tintColor={colors.primary[500]}
+            offTintColor={colors.borderColor}
+            inputCount={4}
+          />
+        </View>
+        <Button
+          style={styles.button}
+          onPress={() => navigation.navigate("EnterDetailsScreen")}
+        >
+          Proceed
+        </Button>
       </View>
-      <Button
-        style={styles.button}
-        onPress={() => navigation.navigate("EnterDetailsScreen")}
-      >
-        Proceed
-      </Button>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
